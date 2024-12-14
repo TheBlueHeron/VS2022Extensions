ecfb8ecb95400002561b65a2273b9265a28c8a5b
Michael Bakker <m.bakker@draaijerpartners.nl>
2024-12-14 22:46:10 +0100

    Add project files.

---
+++ DeleteOutputFolders/Commands/DeleteOutputFoldersCommand.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio;
+
+namespace DeleteOutputFolders;
+
+/// <summary>
+/// The DeleteOutputFolders command handler.
+/// </summary>
+[Command(PackageIds.DeleteOutputFoldersCommand)]
+internal sealed class DeleteOutputFoldersCommand : BaseCommand<DeleteOutputFoldersCommand>
+{
+    #region Objects and variables
+
+    private const string ASTERIX = "*";
+    private const string BIN = "bin";
+    private const string OBJ = "obj";
+    private const string STATUS = "Removing bin/obj folders";
+
+    #endregion
+
+    protected async override Task ExecuteAsync(OleMenuCmdEventArgs e)
+    {
+        await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+        var projects = (await VS.Solutions.GetAllProjectsAsync()).ToList();
+        var outputChannel = Package.GetOutputPane(VSConstants.SID_SVsGeneralOutputWindowPane, "Delete bin/obj folders");
+
+        foreach (var p in projects.ToList())
+        {
+            var idx = 0;
+            var directory = new DirectoryInfo(Path.GetDirectoryName(p.FullPath));
+            var deletables = directory.GetDirectories(ASTERIX, SearchOption.AllDirectories)
+                .Where(d => d.Name.Equals(BIN, StringComparison.InvariantCulture) || d.Name.Equals(OBJ, StringComparison.InvariantCulture)).ToList();
+
+            //await VS.StatusBar.ShowProgressAsync(STATUS, idx, deletables.Count);
+            foreach (var d in deletables)
+            {
+                var fullPath = d.FullName;
+                try
+                {
+                    idx++;
+                    d.Delete(true);
+                    outputChannel.OutputString($"Deleted folder {d.FullName}.\r\n");
+                }
+                catch (IOException ex)
+                {
+                    outputChannel.OutputString($"Error deleting folder {d.FullName}. {ex.Message}\r\n");
+                }
+                finally
+                {
+                    //await VS.StatusBar.ShowProgressAsync(STATUS, idx, deletables.Count);
+                }
+            };
+        };
+    }
+}
---
+++ DeleteOutputFolders/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using DeleteOutputFolders;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(Vsix.Name)]
+[assembly: AssemblyDescription(Vsix.Description)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(Vsix.Author)]
+[assembly: AssemblyProduct(Vsix.Name)]
+[assembly: AssemblyCopyright(Vsix.Author)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion(Vsix.Version)]
+[assembly: AssemblyFileVersion(Vsix.Version)]
+
+#pragma warning disable IDE0161 // Convert to file-scoped namespace
+namespace System.Runtime.CompilerServices
+#pragma warning restore IDE0161 // Convert to file-scoped namespace
+{
+	public class IsExternalInit { }
+}
